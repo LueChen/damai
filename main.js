2db97e9ae83c0a21f9909a58935d5f71a619db4a
Lue <LueChen@foxmail.com>
2018-07-21 17:45:06 +0800

    update for some feature

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-var performid = '8952708'; //日期ID
-var priceid = '11766587'; //价格ID
+var price = '580'; //想要购买的价格
 var person = '2'; //人数
 while (1) {
   var re = main();
@@ -13,9 +12,11 @@ function sleep(n) {
   while (true) if (new Date().getTime() - start > n) break;
 }
 function main() {
+  console.log('====> Begin!!!')
   var buy = document.getElementById('btnBuyNow');
   var Xuanzuo = document.getElementById('btnXuanzuo');
   if (buy !== null) {
+    console.log('====> Clicking buy!!!')
     var Buytext = buy.innerHTML;
     if (Buytext == '立即购买' || Buytext == '立即预定') {
       qiang()
@@ -36,16 +37,20 @@ function main() {
   }
 }
 function qiang() {
+    console.log('====> Start!!!')
+    var priceid;
     var firstperform = document.getElementById('firstperform');
+    var performid = firstperform.getAttribute('data-performid');
     firstperform.className = 'itm  itm-sel';
     var priceList = document.getElementById('priceList').childNodes;
     var children = priceList[3].childNodes[1].childNodes;
-    console.log(children.length);
     for (var i = 0; i < children.length; i++) {
-        if (children[i].getAttribute('data-priceid') == priceid) {
-            console.log(children[i]);
-            children[i].className = 'itm  itm-sel';
-        }
+      // if match the price u want.
+      if (children[i].getAttribute('data-price') == price) {
+          priceid = children[i].getAttribute('data-priceid');
+          console.log(children[i]);
+          children[i].className = 'itm  itm-sel';
+      }
     }
 
     var str = '<div class="ct" style=""><ul class="lst"><li class="itm" style="" data-performid=' + performid + ' data-priceid=' + priceid + '><span class="txt txt-datetime ">"2017-10-28 周六 19:30"</span><span class="txt txt-price">"680"</span><span class="m-nums"><a class="btn btn-low" href="javascript:;">减</a><input class="ipt ipt-num" type="text" value=' + person + '><a class="btn btn-add" href="javascript:;">加</a></span><span class="tips tips-stock"><strong></strong></span><a class="btn btn-del" href="javascript:;"><i></i>删除</a></li></ul></div><div class="ops"><a href="javascript:;" class="u-btn u-btn-c1 u-btn-choose" id="btnXuanzuo" style="">选座购买</a><a class="u-btn u-btn-buynow" href="javascript:;" id="btnBuyNow">立即购买</a></div>'; //测试
